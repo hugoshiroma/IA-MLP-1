a07f35742f71a3672f39b1ff4d3ce880a9fbbfb2
VictorCPPlanas <victplanas@gmail.com>
2020-03-12 15:51:00 -0300

    Leitura de dados do csv

--- src/Main.py
+++ src/Main.py
@@ -1,1 +1,34 @@
-print('Hello world! teste')
+import csv
+
+with open('..\inputs\Part-1\problemAND.csv','rt', encoding="utf-8-sig")as csvAND:
+  arquivoAND = csv.reader(csvAND)
+  print("Arquivo AND")
+  for linha in arquivoAND:
+        print(linha)
+
+
+with open('..\inputs\Part-1\problemOR.csv','rt', encoding="utf-8-sig")as csvOR:
+  arquivoOR = csv.reader(csvOR)
+  print("Arquivo OR")
+  for linha in arquivoOR:
+        print(linha)
+
+
+with open('..\inputs\Part-1\problemXOR.csv','rt', encoding="utf-8-sig")as csvXOR:
+  arquivoXOR = csv.reader(csvXOR)
+  print("Arquivo XOR")
+  for linha in arquivoXOR:
+        print(linha)
+
+
+with open('..\inputs\Part-1\caracteres-limpo.csv','rt', encoding="utf-8-sig")as csvCaracteresLimpo:
+  arquivoCaracteresLimpo = csv.reader(csvCaracteresLimpo)
+  print("Arquivo Caracteres Limpos")
+  for linha in arquivoCaracteresLimpo:
+        print(linha)
+
+with open('..\inputs\Part-1\caracteres-ruido.csv','rt', encoding="utf-8-sig")as csvCaracteresRuido:
+  arquivoCaracteresRuido = csv.reader(csvCaracteresRuido)
+  print("Arquivo Caracteres Ruidosos")
+  for linha in arquivoCaracteresRuido:
+        print(linha)
