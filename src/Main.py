da1f2fbb8d6f227f26b8b706be0d6d254d187645
Isabel <isabel@desincha.com.br>
2020-03-15 19:29:31 -0300

    Proposta de estruturação de código

--- src/Main.py
+++ src/Main.py
@@ -1,11 +1,11 @@
-from src import rede_neural as rn, datasets
+from src import rede_neural as rn, camada_entrada
 
 
-caracteres_limpos = datasets.get_caracteres_limpo()
+caracteres_limpos = camada_entrada.get_caracteres_limpo()
 # cada linha do dataset tem um CONJUNTO de neurônios de entrada, que é o que vai ser mandado pra rede neural para
-# poder classificar essa linha
+# poder treinar essa linha
 for caracter in caracteres_limpos:
-    rn.classificar(caracter)
+    rn.treinar(caracter)
 
 
 
--- src/rede_neural.py
+++ src/rede_neural.py
@@ -1,9 +1,43 @@
+from random import random
+
 
 
 # por enquanto estou considerando que sempre será mandado uma lista onde o último item é a resposta esperada
-def classificar(dado):
+def treinar(dado):
     # basicamente é aqui que vai ficar o algoritmo do MPL
+
     target = dado[-1]
-    neuronios = dado[0:63]
-    print(target, neuronios)
-    # aqui imagino que o classificar devera retornar o valor dos pesos, o treinamento é feito nessa função
+    entradas = dado[:-1]
+
+    # taxa de aprendizado
+    a = random()
+    bias = 1
+    camada_escondida = [1, 2, 3]
+    # ainda não entendi qual será o tamanho dessa lista, coloquei 3 elementos só pra testar
+
+    for x in entradas:
+        # estabelecendo pesos para essa entrada
+        # a qtd de pesos será a qtd de neurônios que existem na camada escondida
+        pesos = []
+        for neuronio_escondido in camada_escondida:
+            pesos.append(random())
+        print(a, target, len(pesos), pesos)
+
+    #print(a, target, entradas)
+    feedforward()
+    backpropagation()
+    ajustar_pesos()
+
+    # aqui imagino que o treinar devera retornar o valor dos pesos, o treinamento é feito nessa função
+
+
+def feedforward():
+    return
+
+
+def backpropagation():
+    return
+
+
+def ajustar_pesos():
+    return
