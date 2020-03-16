adc7d195318fc33dcc28c50d3be956a4e8dc356a
Isabel <isabel@desincha.com.br>
2020-03-15 22:33:06 -0300

    Inicialização de tx de aprendizado da rede, bias e matriz de pesos com números aleatórios

--- src/Main.py
+++ src/Main.py
@@ -1,16 +1,14 @@
-from src import Rede as rn, camada_entrada
+from src import Rede, camada_entrada
 from src.helpers.Mapper import Mapper
 
-
+tam_camada_escondida = 2
+tam_camada_saida = 7
+num_epocas = 100
 caracteres_limpos = camada_entrada.get_caracteres_limpo()
-# cada linha do dataset tem um CONJUNTO de neurônios de entrada, que é o que vai ser mandado pra rede neural para
-# poder treinar essa linha
-for caracter in caracteres_limpos:
-    rn.treinar(caracter)
 
-test = Mapper()
-print(test.files[1]['value'])
+rn = Rede.Rede(caracteres_limpos, tam_camada_escondida, tam_camada_saida, num_epocas)
+rn.treinar()
 
-
-
+# test = Mapper()
+# print(test.files[1]['value'])
 
--- src/Rede.py
+++ src/Rede.py
@@ -1,11 +1,49 @@
-from random import random
-import numpy as np
+from numpy import random
 
 
-class Rede(object):
-    def __init__(self, sizes):
-        self.num_camadas = len(sizes)
-        self
+class Rede:
+    def __init__(self, camada_entrada, tam_camada_escondida, tam_camada_saida, num_epocas):
+        self.a = random.random()
+        self.camada_entrada = camada_entrada
+        self.tam_camada_escondida = tam_camada_escondida
+        self.tam_camada_saida = tam_camada_saida
+        self.num_epocas = num_epocas
+
+    # por enquanto estou considerando que sempre será mandado uma lista onde o último item é a resposta esperada
+    def treinar(self):
+        # basicamente é aqui que vai ficar o algoritmo do MPL
+        caracteres = self.camada_entrada
+        # coloquei nomes específicos ao nosso problema, mas podemos mudar pra nomes mais genéricos dps
+
+        # para cada par de treinamento s: t
+        for caracter in caracteres:
+            target = caracter[-1] #t
+            entradas = caracter[:-1] #s
+
+            bias = random.random()
+            matriz_pesos = [] # a matriz aqui será uma lista de listas
+            for x in range(len(entradas)):
+                pesos = []
+                for neuronio_escondido in range(self.tam_camada_escondida):
+                    pesos.append(random.random())
+                matriz_pesos.append(pesos)
+                # na linha acima, colocamos os pesos para cada entrada x em relacao ao neuronio i da cama_escondida
+
+            print("Taxa de aprendizado da rede:", self.a)
+            print("Target:", target)
+            print("Bias:", bias)
+            print("Matriz de pesos:")
+            i = 1
+            for pesos in matriz_pesos:
+                print(i, pesos)
+                i += 1
+            print()
+
+            self.feedforward()
+            self.backpropagation()
+            self.ajustar_pesos()
+
+        # aqui imagino que o treinar devera retornar o valor dos pesos, o treinamento é feito nessa função
 
     def feedforward(self):
         return
@@ -16,33 +54,6 @@ class Rede(object):
     def ajustar_pesos(self):
         return
 
-    # por enquanto estou considerando que sempre será mandado uma lista onde o último item é a resposta esperada
-    def treinar(dado):
-        # basicamente é aqui que vai ficar o algoritmo do MPL
-
-        target = dado[-1]
-        entradas = dado[:-1]
-
-        # taxa de aprendizado
-        a = random()
-        bias = 1
-        camada_escondida = [1, 2, 3]
-        # ainda não entendi qual será o tamanho dessa lista, coloquei 3 elementos só pra testar
-
-        for x in entradas:
-            # estabelecendo pesos para essa entrada
-            # a qtd de pesos será a qtd de neurônios que existem na camada escondida
-            pesos = []
-            for neuronio_escondido in camada_escondida:
-                pesos.append(random())
-            print(a, target, len(pesos), pesos)
-
-        #print(a, target, entradas)
-        feedforward()
-        backpropagation()
-        ajustar_pesos()
-
-        # aqui imagino que o treinar devera retornar o valor dos pesos, o treinamento é feito nessa função
 
 
 
